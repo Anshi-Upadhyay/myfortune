7a8512f49d74af9639bd17809d106144c4aff505
Anshi-Upadhyay <thequeenanshi@gmail.com>
2026-03-08 10:48:19 +0530

    Create index.js

---
+++ src/data/translations/index.js
@@ -0,0 +1,17 @@
+import en from './en';
+import hi from './hi';
+import es from './es';
+import fr from './fr';
+import it from './it';
+import ko from './ko';
+
+const translations = {
+  en,
+  hi,
+  es,
+  fr,
+  it,
+  ko,
+};
+
+export default translations;
